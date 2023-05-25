dd7536fda53c331bfb13b24e67b31fa8106e1e51
Степан Крылов <kiriki00@mail.ru>
2023-05-25 18:41:37 +0700

    action tests

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from .views import (
     CategoriesViewSet,
-    CategoriesViewSet,
+    CommentViewSet,
     GenresViewSet,
     ReviewViewSet,
     TitlesViewSet
